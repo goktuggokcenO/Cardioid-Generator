82842cb8d3379a0badc6d60a35e8334ee4c8390f
Göktuğ Gökcen <goktuggokcen0@gmail.com>
2024-10-05 18:24:40 +0300

    Add Cardioid class and draw cardioid shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Libraries.
 import pygame as pg
+import math
 
 
 # Application class.
@@ -8,10 +9,12 @@ class App:
     def __init__(self):
         self.screen = pg.display.set_mode((800, 600))
         self.clock = pg.time.Clock()
+        self.cardioid = Cardioid(self)
 
     # Draw method.
     def draw(self):
         self.screen.fill("black")
+        self.cardioid.draw()
         pg.display.flip()
 
     # Run method.
@@ -24,6 +27,32 @@ class App:
             self.draw()
             self.clock.tick(60)
         pg.quit()
+
+
+# Cardioid class.
+class Cardioid:
+    # Constructor.
+    def __init__(self, app):
+        self.app = app
+        self.radius = 250
+        self.num_points = 200
+        self.translate = (
+            self.app.screen.get_width() // 2,
+            self.app.screen.get_height() // 2,
+        )
+
+    # Draw method.
+    def draw(self):
+        for i in range(self.num_points):
+            theta = (2 * math.pi / self.num_points) * i
+
+            x1 = int(self.radius * math.cos(theta)) + self.translate[0]
+            y1 = int(self.radius * math.sin(theta)) + self.translate[1]
+
+            x2 = int(self.radius * math.cos(2 * theta)) + self.translate[0]
+            y2 = int(self.radius * math.sin(2 * theta)) + self.translate[1]
+
+            pg.draw.line(self.app.screen, "green", (x1, y1), (x2, y2))
 
 
 # Check if the file is being run directly.
